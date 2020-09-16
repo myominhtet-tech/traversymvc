c07d951701354cf99434e339a121ff41fccb87b9
myomin <myominhtet.cumtl@gmail.com>
2020-09-17 01:15:31 +0630

    first commit

---
+++ app/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+//load config
+require_once 'config/config.php';
+//load libraries 
+// require_once 'libraries/core.php';
+// require_once 'libraries/controller.php';
+// require_once 'libraries/database.php';
+
+//Autoload Core Libraries
+spl_autoload_register(function($className){
+    require_once 'libraries/'. $className . '.php';});
